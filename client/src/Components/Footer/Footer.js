6032227bda9abccf114901c2e6c66ef754c84981
unknown <andrelopessfla@gmail.com>
2020-05-10 14:30:05 -0300

    tarefa de hoje finalizada

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import './footer.css';
+import { Link } from 'react-router-dom';
 
 export default class Footer extends Component {
     render() {
         return (
             <div>
                 <div className="pe row">
+
                    <div className="col-4">
                        <h6 id="peh6">
                        Sigmed - Um SoftWare de Organização e gerenciamento 
                        de consultórios e clínicas Médicas
                        </h6>
                    </div>
-                   <div className="col-4"></div>
+                   <div className="col-4">
+                   <Link to="">  <img className="ico" src="img/inst.png" alt=""/></Link>
+                   <Link to= ""><img  className="ico" src="img/face.png" alt=""/></Link>
+                   <Link to=""> <img className="ico" src="img/whats.png" alt=""/></Link>
+                 
+                        
+                       
+
+                   </div>
                    <div className="col-4">
                         <h6 id="peh6">
                         Termo de uso | Política de privacidade <br></br>
                      ©2020 SigMed
                         </h6>
                    </div>
+
                 </div>
            
             </div>
